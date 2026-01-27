fb75b6f3e0c46391753a6e948b48ae09cc039f4b
ericdaetz <ericdaetz@gmail.com>
2026-01-27 12:19:19 -0800

    Initialization

---
+++ organizer.py
@@ -0,0 +1,21 @@
+import os
+
+class FileOrganizer:
+
+    types_dict = {
+        "Documents" = [".doc", ".docx", ".pdf", ".txt"],
+
+        "Videos" = [".wmv", ".mp4" ".mov", ".mpg", ".mpeg", ".mkv"]
+
+        "Music" = [".mp3", ".wav", ".wma", ".msv"]
+
+        
+    }
+
+    def organizer(self):
+        
+        
+
+if __name__ == '__main__':
+    my_organizer = FileOrganizer()
+    my_organizer.organizer()
