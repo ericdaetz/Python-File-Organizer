741d0beac0a74208f42be4b650a7b63adcb85fd2
ericdaetz <ericdaetz@gmail.com>
2026-01-27 12:37:33 -0800

    Fixing some early issues, shaking off rust

--- organizer.py
+++ organizer.py
@@ -1,18 +1,23 @@
 import os
+import argparse
 
 class FileOrganizer:
 
-    types_dict = {
-        "Documents" = [".doc", ".docx", ".pdf", ".txt"],
+    types_dict = None
 
-        "Videos" = [".wmv", ".mp4" ".mov", ".mpg", ".mpeg", ".mkv"]
+    #Non-listed types go into Miscellaneous Folder
+    def __init__(self):
+        self.types_dict = {
+            "Documents": [".doc", ".docx", ".pdf", ".txt"],
 
-        "Music" = [".mp3", ".wav", ".wma", ".msv"]
+            "Videos": [".wmv", ".mp4" ".mov", ".mpg", ".mpeg", ".mkv"],
 
-        
-    }
+            "Music": [".mp3", ".wav", ".wma", ".msv"]
+        }
+    
 
     def organizer(self):
+        print(self.types_dict)
         
         
 
